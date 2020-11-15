a33955566a463ca00d64d8bd76a224559a054a84
Nguyen <trungminhtri.nguyen@my.ccsu.edu>
2020-11-15 01:15:26 -0500

    creepy abstractfactory

--- AbstractFactory.java
+++ AbstractFactory.java
@@ -1,6 +1,6 @@
 /**
- * This class defines factory method and provides user with abstract method.
- * It is used to create a CellObject (tower or enemy).
+ * This class defines factory method and provides user with abstract method. It
+ * is used to create a CellObject (tower or enemy).
  */
 public interface AbstractFactory {
     public CellComponent createProduct(String cellObject, Cell cell);
